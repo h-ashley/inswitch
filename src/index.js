15186b7e74fcac01f82cd664504e4e302155f655
Huw Ashley <Huw.Ashley@gocompare.com>
2019-10-06 21:21:09 +0100

    Input switching

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
-export default class DateInputHelper {
+export default class Inswitch {
+    /**
+     * @param {String} containerSelector
+     */
+    constructor(containerSelector) {
+        document.addEventListener('DOMContentLoaded', () => {
+            this.setup(containerSelector);
+        }, false);
+    }
+
+    /**
+     * @param {String} containerSelector
+     */
+    setup(containerSelector) {
+        this.container = document.querySelector(containerSelector);
+        this.inputs = this.container.querySelectorAll('input');
+        for (let i = 0; i < this.inputs.length; i++) {
+            this.inputs[i].dataset.inswitchId = i;
+            this.inputs[i].addEventListener("keydown", Inswitch.handleInput, false);
+        }
+    }
+
     /**
      * @param {KeyboardEvent} event
      * @return {boolean}
@@ -24,8 +45,9 @@ export default class DateInputHelper {
     static focusOnNextInput(event)
     {
         if (!['Backspace','Tab','Delete'].includes(event.key) && event.target.value.length >= event.target.maxLength) {
-            if (event.target.dataset.nextId) {
-                document.getElementById(event.target.dataset.nextId).focus();
+            let nextId = parseInt(event.target.dataset.inswitchId) + 1;
+            if (nextId < 3) {
+                document.querySelector('[data-inswitch-id="'+nextId+'"]').focus();
             }
         }
     }
@@ -36,8 +58,9 @@ export default class DateInputHelper {
      */
     static focusOnPreviousInput(event)
     {
-        if (event.target.dataset.previousId) {
-            document.getElementById(event.target.dataset.previousId).focus();
+        let previousId = parseInt(event.target.dataset.inswitchId) - 1;
+        if (previousId >= 0) {
+            document.querySelector('[data-inswitch-id="'+previousId+'"]').focus();
         }
     }
 
@@ -45,16 +68,16 @@ export default class DateInputHelper {
      * @param {KeyboardEvent} event
      * @return {void}
      */
-    static handleDobInput(event) {
-        if (!DateInputHelper.isNumericOrSpecialKey(event)) {
-            DateInputHelper.dropLastKeystroke(event);
+    static handleInput(event) {
+        if (!Inswitch.isNumericOrSpecialKey(event)) {
+            Inswitch.dropLastKeystroke(event);
         }
         // If input is empty and backspace is pressed, go to previous input if applicable
         if (event.key === 'Backspace' && event.target.value.length === 0) {
-            DateInputHelper.focusOnPreviousInput(event);
+            Inswitch.focusOnPreviousInput(event);
         } else {
             // If input reaches maxlength, go to next input if applicable
-            DateInputHelper.focusOnNextInput(event);
+            Inswitch.focusOnNextInput(event);
         }
     }
 }
